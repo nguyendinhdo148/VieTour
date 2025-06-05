17c4d655a51144f94d07a10faf487ad9c02fbaba
Gia Long <lygialong201@gmail.com>
2025-06-05 20:48:12 +0700

    docker

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -11,4 +11,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true, // Để truy cập từ máy host
+    watch: {
+      usePolling: true, // Bắt buộc với Docker để theo dõi thay đổi file
+    },
+  },
 });
