057d8f461a410ff69532e137b4d38d0d96ec0bab
Gia Long <lygialong201@gmail.com>
2025-10-03 01:34:09 +0700

    fix

--- fe/src/utils/constant.ts
+++ fe/src/utils/constant.ts
@@ -1,4 +1,4 @@
 export const API =
   import.meta.env.MODE === "development"
     ? "http://localhost:8000/api/v1"
-    : "https://vie-jobs-d56h.vercel.app/api/v1";
+    : "https://viejobs-be.onrender.com/api/v1";
