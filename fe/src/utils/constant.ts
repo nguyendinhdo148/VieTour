2e9ee6cc7855e27761a1314383f1055463dca5d8
Gia Long <lygialong201@gmail.com>
2025-10-03 01:28:29 +0700

    fix api for deploy web

--- fe/src/utils/constant.ts
+++ fe/src/utils/constant.ts
@@ -1,1 +1,4 @@
-export const API = "http://localhost:8000/api/v1";
+export const API =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8000/api/v1"
+    : "https://vie-jobs-d56h.vercel.app/api/v1";
