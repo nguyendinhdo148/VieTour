3e662ce953f55478eccd9a325b0729bda2b81ea6
nguyendinhdo148 <nguyendinhdo2k7@gmail.com>
2026-06-07 19:42:50 +0700

    1

--- fe/src/types/company.ts
+++ fe/src/types/company.ts
@@ -1,31 +1,32 @@
 import { User } from "./user";
 
 export interface Company {
-  featuredImages: any;
-  featuredImages: Company | null | undefined;
+  featuredImages?: string[];
+
   _id: string;
   name: string;
   slug: string;
-  description?: string; // Optional description
-  website?: string; // Optional website URL
-  location?: string; // Optional location
-  address?: string; // Optional address
-  logo?: string; // Optional logo URL
-  businessLicense?: string; // Optional business license URL
-  taxCode?: string; // Optional tax code
-  noe?: string; // number of employees
-  yoe?: string; // years of experience
-  field?: string; // field of work
-  email?: string; // Email của công ty/người đại diện
-  phoneNumber?: string; // Số điện thoại liên hệ
-  approval?: string; // "pending" | "approved" | "rejected"
-  approvalNote?: string; // Ghi chú từ admin
-  userId: User; // ID của người dùng tạo công ty
-  createdAt: string; // ISO date string
-  updatedAt: string; // ISO date string
+  description?: string;
+  website?: string;
+  location?: string;
+  address?: string;
+  logo?: string;
+  businessLicense?: string;
+  taxCode?: string;
+  noe?: string;
+  yoe?: string;
+  field?: string;
+  email?: string;
+  phoneNumber?: string;
+  approval?: string;
+  approvalNote?: string;
+  userId: User;
+  createdAt: string;
+  updatedAt: string;
+
   geolocation?: {
     type: "Point";
-    coordinates: number[]; // [lng, lat]
+    coordinates: number[];
   };
 
   distance?: number;
